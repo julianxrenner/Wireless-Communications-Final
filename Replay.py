2e119f9cc4f9236c77423958273d55f4db3dfe61
julianxrenner <julesman18@gmail.com>
2025-05-07 15:09:13 -0400

    Replay checks for correct interface

--- Replay.py
+++ Replay.py
@@ -1,5 +1,23 @@
 from scapy.all import rdpcap, sendp
 import os
+import platform
+import subprocess
+# File with malicious packets = output_chunk_00306_20201213135929.pcap
+
+def get_loopback_interface():
+    os_type = platform.system()
+
+    if os_type == "Linux":
+        return "lo"
+    elif os_type == "Darwin": 
+        return "lo0"
+    elif os_type == "Windows":
+        return "Loopback" 
+    else:
+        raise Exception("Unsupported OS")
+
+# Example usage
+loopback_iface = get_loopback_interface()
 
 
 filename = input("Input a filename or enter '.' to loop through all files: ")
@@ -9,7 +27,7 @@ if filename == '.':
     for item in os.listdir(path)[500::]:
         print(item)
         packets = rdpcap(f"./edit_pcaps/{item}")
-        sendp(packets, iface="lo0", verbose=True)
+        sendp(packets, iface=loopback_iface, verbose=True)
 else:
     packets = rdpcap(f"./edit_pcaps/{filename}")
-    sendp(packets, iface="lo0", verbose=True)
+    sendp(packets, iface=loopback_iface, verbose=True)
